c5d3b764ca3aed34b1f6fcef209fe5f9085389f0
Mariah Wear <wearmariahk@gmail.com>
2023-05-28 10:37:00 -0600

    trying to make direction to thank you page work

--- src/components/thankyou.js
+++ src/components/thankyou.js
@@ -4,7 +4,7 @@ function ThankYou() {
     return (
     <>
     Thank you for your submission!!!
-    <a href="#About">Click to return to my portfolio</a>
+    <a href="https://mariahw4.github.io/react-portfolio/#About-Me">Click to return to my portfolio</a>
     </>
     )
 }
